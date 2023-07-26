6aa48990229615cd6323e9d024d37ab27afada44
MaxSieniawski05 <maxjoseph719@gmail.com>
2023-07-26 17:48:41 -0300

    função 1

--- EP2dp.py
+++ EP2dp.py
@@ -1,1 +1,14 @@
-print(2)
+def transforma_base(questions):
+    questions_by_level = {}  
+
+    for question in questions:
+        level = question.get('nivel')
+
+        if level is not None:
+            if level in questions_by_level:
+                questions_by_level[level].append(question)
+            else:
+                questions_by_level[level] = [question]
+
+    return questions_by_level
+
