470a8fc258b4aba7e9af4e00ed1e75cfdc5d3aac
MaxSieniawski05 <maxjoseph719@gmail.com>
2023-07-27 08:09:51 -0300

    Função 2

--- EP2dp.py
+++ EP2dp.py
@@ -12,3 +12,43 @@ def transforma_base(questions):
 
     return questions_by_level
 
+def valida_questao(questao):
+    d = {}
+
+    if 'titulo' not in questao:
+        d['titulo'] = 'nao_encontrado'
+    elif not questao['titulo'].strip():
+        d['titulo'] = 'vazio'
+    
+    if 'nivel' not in questao:
+        d['nivel'] = 'nao_encontrado'
+    elif questao['nivel'] not in {'facil', 'medio', 'dificil'}:
+        d['nivel'] = 'valor_errado'
+
+    
+    if 'opcoes' not in questao:
+        d['opcoes'] = 'nao_encontrado'
+    elif len(questao['opcoes']) != 4:
+        d['opcoes'] = 'tamanho_invalido'
+    else:
+        d2 = {}
+        for opcao in 'ABCD':
+            if opcao not in questao['opcoes']:
+                d2[opcao] = 'chave_invalida_ou_nao_encontrada'
+            elif not questao['opcoes'][opcao].strip():
+                d2[opcao] = 'vazia'
+
+        if d2:
+            d['opcoes'] = d2
+
+    
+    if 'correta' not in questao:
+        d['correta'] = 'nao_encontrado'
+    elif questao['correta'] not in 'ABCD':
+        d['correta'] = 'valor_errado'
+
+    
+    if len(questao) != 4:
+        d['outro'] = 'numero_chaves_invalido'
+
+    return d
